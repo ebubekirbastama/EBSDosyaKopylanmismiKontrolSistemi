852bdc2f9a4717d049b423da259fe41b01c7acee
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2024-02-24 11:38:23 +0300

    Add files via upload

---
+++ DosyaKopylandımıKontrolü/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DosyaKopylandımıKontrolü
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(662, 316);
+            this.splitContainer1.SplitterDistance = 104;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Log Kontrol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(554, 316);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(662, 316);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kopyalama Kontrolü";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DosyaKopylandımıKontrolü/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DosyaKopylandımıKontrolü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread th;
+
+        private void logkontrol()
+        {
+            // Güvenlik günlüğündeki olayları izleyeceğiz
+            string logName = "Security";
+
+            // Belirli bir olay tipini filtrelemek için kullanılacak anahtar kelime
+            string eventKeyword = "4663"; // Bu örnekte, dosya/folder erişim olaylarını temsil eder
+
+            // EventLog örneği oluşturun
+            EventLog eventLog = new EventLog(logName);
+
+            // Olayları filtreleme
+            EventLogEntryType entryType = EventLogEntryType.SuccessAudit; // Başarılı denetim olayları
+            EventLogEntryCollection entries = eventLog.Entries;
+            int sayac = 1;
+
+            // Güvenlik günlüğündeki yeni olayları kontrol etme
+            foreach (EventLogEntry entry in entries)
+            {
+
+                // Sadece belirli olay tipini ve anahtar kelimeyi içeren olayları işle
+                if (entry.EntryType == entryType && entry.Message.Contains(eventKeyword))
+                {
+                    listBox1.Items.Add($"Dosya kopyalama olayı: {entry.TimeGenerated} - {entry.Message}");
+                }
+                label1.Text = "Analiz edilen : " + sayac.ToString();
+                sayac++;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            th = new Thread(logkontrol); th.Start();
+        }
+    }
+}
